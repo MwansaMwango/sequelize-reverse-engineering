ed6e8390907839eda4c520e3de395a12be3f7097
Mwansa <Mwansad.mwango@gmail.com>
2020-05-28 00:25:12 +0800

    Rearranged folder structure

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function(req, res) { // runs passport middleware using local strategy
     res.json(req.user);
   });
 
@@ -22,7 +22,7 @@ module.exports = function(app) {
         res.redirect(307, "/api/login");
       })
       .catch(function(err) {
-        res.status(401).json(err);
+        res.status(401).json(err); // returns user not allowed 401
       });
   });
 
