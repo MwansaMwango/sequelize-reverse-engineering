b6f0cdb3c9abe9a57015540d152fd9c51cd3da72
Mwansa <Mwansad.mwango@gmail.com>
2020-05-30 11:36:06 +0800

    Updated minor commentary

--- Develop/server.js
+++ Develop/server.js
@@ -13,18 +13,35 @@ var app = express(); // initialize express server app
 app.use(express.urlencoded({ extended: true })); // parses incoming requests url and allows values of any type instead of just strings.
 app.use(express.json()); // parses requests into JSON
 app.use(express.static("public")); // allows access to the static files in the public folder for client side.
+
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true })); // Not recommended for secuity reasons should be saved elsewhere .env files.
+app.use(
+  session({ 
+    secret: "keyboard cat", //Secret used to sign the session ID cookie. Can be either a string for a single secret, or an array of multiple secrets.
+    resave: true, //Forces the session to be saved back to the session store, even if the session was never modified during the request. Avoid the user resigning
+    //in on every new page request.
+    saveUninitialized: true // Forces a session that is “uninitialized” to be saved to the store. Saves a session even when user is not logged in.
+  })
+);
+
+// Not recommended for secuity reasons should be
+//saved elsewhere .env files.
 app.use(passport.initialize()); // initializes an instance of passport
-app.use(passport.session()); // begins session control of passport
+app.use(passport.session()); // middleware that changes the req object's 'user' value from session id into the deserialized i.e.
+// 'authenticated' user object
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() { // syncronizes database and established connection
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function () {
+  // syncronizes database and established connection
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
